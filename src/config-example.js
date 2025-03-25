3c402e76481fb1f25b9c26b673f66eefbb269236
Luka T <l.tsiklauri@itmegainsight.com>
2025-03-26 00:19:15 +0400

    config-example Aktualisierung

--- src/config-example.js
+++ src/config-example.js
@@ -1,11 +1,14 @@
 import { randomBytes } from 'crypto';
 
+/**
+ * Trading websocket configurations
+*/
 export const wsUrlDemo = 'wss://demo-api-eu.po.market/socket.io/?EIO=4&transport=websocket';
-export const wsUrlReal = // [ WEBSOCKET URL FOR REAL ]; EU/RUS, US NA or US SA servers based on your location
+export const wsUrlReal = // [ WEBSOCKET URL FOR REAL ];
 
 export const authMessageDemo = // [ WEBSOCKET AUTH MESSAGE FOR REAL DEMO ];
 export const authMessageReal = // [ WEBSOCKET AUTH MESSAGE FOR REAL ACCOUNT ];
-// wsUrlReal example: `42["auth",{"session":"a:4:{s:10:\\\"session_id\\\";s:32:\\\"086dcefsdfd01eweffwefwf1ee956fd\\\";s:10:\\\"ip_address\\\";s:12:\\\"185.1.1.1\\\";s:10:\\\"user_agent\\\";s:117:\\\"Mozilla/5.0 (Macintosh; Intel Mac OS X 14_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.3 Safari/605.1.15\\\";s:13:\\\"last_activity\\\";i:1442896190;}b4912fd6341fqeq1231239d6e6fe796b778115eb","isDemo":0,"uid":84890061,"platform":1}]`;
+// authMessageReal example: `42["auth",{"session":"a:4:{s:10:\\\"session_id\\\";s:32:\\\"XXXXXXXXXXXXXXXXXXX\\\";s:10:\\\"ip_address\\\";s:12:\\\"185.1.1.1\\\";s:10:\\\"user_agent\\\";s:117:\\\"Mozilla/5.0 (Macintosh; Intel Mac OS X 14_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.3 Safari/605.1.15\\\";s:13:\\\"last_activity\\\";i:1442896190;}b4912fd6341fqeq1231239d6e6fe796b778115eb","isDemo":0,"uid":84890061,"platform":1}]`;
 // Use 3 backslashes "\\\" 
 
 const generateSecWebSocketKey = () => {
@@ -26,3 +29,23 @@ export const headers = {
     'Upgrade': 'websocket',
     'User-Agent': // [ USER AGENT DATA ]
 };
+
+
+
+
+/**
+ * Chat websocket configurations
+*/
+export const wsChatUrl = 'wss://chat-po.site/cabinet-client/socket.io/?EIO=4&transport=websocket'
+export const userInit = // [ WEBSOCKET CHAT AUTH MESSAGE ];
+// userInit example: '42["user_init",{"id":000000,"secret":"XXXXXXXXXXX"}]'
+
+
+
+
+/**
+ * Events websocket configurations
+*/
+export const wsEventsUrl = 'wss://events-po.com/socket.io/?EIO=4&transport=websocket'
+export const sessionToken = // [ WEBSOCKET EVENTS AUTH MESSAGE ];
+// sessionToken example: '42["auth",{"sessionToken":"XXXXXXXXXXX","uid":000000}]'
